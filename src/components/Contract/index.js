d85c16e499e6080c804ba35757ca0c388fa7a16c
acrdlph <achill@way.network>
2018-03-28 15:16:01 +0200

    Get Acc data from eth

--- src/components/Contract/index.js
+++ src/components/Contract/index.js
@@ -6,7 +6,7 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 function getContractConnection() {
     const WeeveRepCoinContract = web3.eth.contract(WeeveRepCoinABI);
-    const weeveRepoCoinContractInstance = WeeveRepCoinContract.at('0xc0ed63e3a70bfcb003452b1cc083db822e1f23e1');
+    const weeveRepoCoinContractInstance = WeeveRepCoinContract.at('0x1ee1dba36bed02f6a43d2019610bb70a25d447d3');
 
     console.log('contractInstance', weeveRepoCoinContractInstance);
     return weeveRepoCoinContractInstance;
