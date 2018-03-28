9e04c6066fac8ddb735f7e26b38728435000b342
Christian Haug <csh@christianhaug.com>
2018-03-28 15:34:55 +0200

    Use local config for contract address

--- src/components/Contract/index.js
+++ src/components/Contract/index.js
@@ -1,12 +1,14 @@
 import Web3 from 'web3';
 
+import { WEEVE_REP_COIN_CONTRACT_ADDRESS } from '../../config';
 import WeeveRepCoinABI from './WeeveRepCoinABI';
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 function getContractConnection() {
     const WeeveRepCoinContract = web3.eth.contract(WeeveRepCoinABI);
-    const weeveRepoCoinContractInstance = WeeveRepCoinContract.at('0x1ee1dba36bed02f6a43d2019610bb70a25d447d3');
+    const weeveRepoCoinContractInstance = WeeveRepCoinContract.at(WEEVE_REP_COIN_CONTRACT_ADDRESS);
+
 
     console.log('contractInstance', weeveRepoCoinContractInstance);
     return weeveRepoCoinContractInstance;
---
+++ src/config.sample.js
@@ -0,0 +1,1 @@
+export const WEEVE_REP_COIN_CONTRACT_ADDRESS = 'enter_local_contract_address_here';
